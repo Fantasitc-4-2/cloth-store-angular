7480a9d589eb5d986df356e7ed09cf998f2b9fb2
Zeyad Mohamed <ziadmohomar123@gmail.com>
2025-10-21 23:10:53 +0300

    added profile protected route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { Home } from './features/home/home';
 import { Login } from './features/login/login';
 import { Register } from './features/register/register';
 import { ProductList } from './features/products/product-list/product-list';
+import { Profile } from './features/profile/profile';
+import { AuthGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
     {
@@ -20,5 +22,10 @@ export const routes: Routes = [
     {
         path: "products",
         component: ProductList
+    },
+    {
+        path: "profile",
+        component: Profile,
+        canActivate: [AuthGuard]
     }
 ];
