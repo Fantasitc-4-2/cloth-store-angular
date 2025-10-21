a7842b6e049138c5e045909a260df3dfa128828c
Nour <kkid607@gmail.com>
2025-10-21 15:05:02 +0300

    cart route + initiated cart page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,20 +5,24 @@ import { Register } from './features/register/register';
 import { ProductList } from './features/products/product-list/product-list';
 
 export const routes: Routes = [
-    {
-        path: "",
-        component: Home
-    },
-    {
-        path: "login",
-        component: Login
-    },
-    {
-        path: "register",
-        component: Register
-    },
-    {
-        path: "products",
-        component: ProductList
-    }
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'login',
+    component: Login,
+  },
+  {
+    path: 'register',
+    component: Register,
+  },
+  {
+    path: 'products',
+    component: ProductList,
+  },
+  {
+    path: 'cart',
+    loadComponent: () => import('./features/cart/cart').then((m) => m.Cart),
+  },
 ];
