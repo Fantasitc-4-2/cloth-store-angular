7d2da17b2732705692701f31c21443b50802fc8b
mohammed Khaled <72397920+mohammed-khaled-5@users.noreply.github.com>
2025-10-22 12:41:49 +0300

    add product card to the products page

--- src/app/shared/product-card/product-card.ts
+++ src/app/shared/product-card/product-card.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-card',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-card.html',
-  styleUrl: './product-card.css'
+  styleUrls: ['./product-card.css']
 })
 export class ProductCard {
-
+  @Input() product: any;
 }
