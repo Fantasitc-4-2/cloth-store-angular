96c9ab473c93e8c3350752857ebd0745f5c66209
mohammed Khaled <72397920+mohammed-khaled-5@users.noreply.github.com>
2025-10-22 14:50:57 +0300

    fetching all products and connecting it with product details

--- src/app/features/products/product-list/product-list.ts
+++ src/app/features/products/product-list/product-list.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Sidebar } from "../../../shared/sidebar/sidebar";
 import { ProductCard } from "../../../shared/product-card/product-card";
 import { ProductSearch } from "../product-search/product-search";
+import { ProductService } from '../../../core/services/product.service';
+import { Product } from '../../../models/product.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
@@ -10,41 +13,32 @@ import { ProductSearch } from "../product-search/product-search";
   templateUrl: './product-list.html',
   styleUrls: ['./product-list.css']
 })
-export class ProductList {
+export class ProductList implements OnInit{
 
 
-  products = [
-  {
-    title: 'Full Sleeve Zipper',
-    category: 'Cotton T Shirt',
-    price: 199,
-    image: 'images/1.jpg'
-  },
-  {
-    title: 'Basic Slim Fit T-Shirt',
-    category: 'Cotton T Shirt',
-    price: 129,
-    image: 'images/2.jpg'
-  },
-  {
-    title: 'Basic Slim Fit T-Shirt',
-    category: 'Cotton T Shirt',
-    price: 129,
-    image: 'images/3.jpg'
-  },
-  {
-    title: 'Basic Slim Fit T-Shirt',
-    category: 'Cotton T Shirt',
-    price: 129,
-    image: 'images/4.jpg'
-  },
-  {
-    title: 'Basic Slim Fit T-Shirt',
-    category: 'Cotton T Shirt',
-    price: 129,
-    image: 'images/5.jpg'
+
+ products: Product[] = [];
+
+ constructor(private productService: ProductService,private router: Router) {}
+ngOnInit(): void {
+    this.productService.getAllProducts().subscribe({
+      next: (data) => {
+        this.products = data;
+        console.log('Products loaded:', data);
+      },
+      error: (err) => {
+        console.error('Error loading products:', err);
+      }
+    });
   }
-];
+
+  goToProduct(id?: string) {
+  if (!id) {
+    console.error('Product ID is missing!');
+    return;
+  }
+  this.router.navigate(['/products', id]);
+}
 
 
 }
