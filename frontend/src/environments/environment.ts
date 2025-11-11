d3137a1677cec763cd7e1ca506b7cc7aa0632104
neggmmm <kekkdead@gmail.com>
2025-11-11 13:56:51 +0200

    change api call

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000',
+  apiUrl: 'https://cloth-store-angular-vert.vercel.app/',
   GOOGLE_CLIENT_ID: '576816836547-ba0hv9i0hjfibacr81mvv1arc4851sv6.apps.googleusercontent.com',
 };
